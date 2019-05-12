966497feaec38ebb69c714a271a3cfffd987e3c6
Malhar Rinpoche <malhar@Malhars-MacBook-Air.local>
2019-05-12 00:25:22 -0700

    commented integration test

---
+++ integration_tests/commented_command_tests.sh
@@ -0,0 +1,35 @@
+INPUTS=(
+    "echo hello; #echo comment"
+    "#echo comment"
+    "echo bye && #echo comment"
+    "echo testing; #echo comment; echo hello"
+)
+OUTPUTS=(
+    "hello"
+    ""
+    "bye"
+    "testing"
+)
+
+LENGTH=${#INPUTS[*]}
+LENGTH="$(($LENGTH-1))"
+for i in $(seq 0 $LENGTH)
+do
+    INPUT="${INPUTS[${i}]}"
+    EXPECTEDOUTPUT="${OUTPUTS[${i}]}"
+    OUTPUT=$(../rshell "$INPUT")
+    OUTPUT=${OUTPUT//$'\n'/}
+    echo "Input:" $INPUT
+    echo "Output:" "${OUTPUT}"
+    echo "EXPECTEDOUTPUT:" "${EXPECTEDOUTPUT}"
+
+
+    if [ "${OUTPUT}" = "${EXPECTEDOUTPUT}" ]
+    then
+        echo "Test passed"
+    else
+        echo "Test failed"
+    fi
+    printf "\n"
+
+done
