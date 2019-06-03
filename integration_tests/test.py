fbb9b0bd104374e10c20d345454e77060cfd395e
AriAlavi <arianalavi1@gmail.com>
2019-06-03 00:43:11 -0700

    Comments now work in the integration tests

--- integration_tests/test.py
+++ integration_tests/test.py
@@ -4,7 +4,7 @@ from json import load as jsonLoad
 from itertools import product
 import re
 import unittest
-
+from secrets import token_hex
 
 COMPILED_CPLUS = "../rshell"
 
@@ -24,6 +24,55 @@ def runCommand(command): #takes in an a command and runs it in the base compiled
 
 class DynamicTest(unittest.TestCase):
     ...
+
+class BaseCommand():
+    command = ""
+    outputType = ""
+    POSSIBLE_OUTPUTS = ["Pass", "Fail", "End"]
+
+    def getOutput(self, command):
+
+        def generateStringNotInString(givenString):
+            foundString = token_hex(16)
+            while foundString in givenString:
+                foundString = token_hex(16)
+            return foundString
+
+        def testPass(command, lookFor):
+            newCommand = command + " && " + "echo " + lookFor
+            if lookFor in runCommand(newCommand):
+                return True
+            return False
+        def testFail(command, lookFor):
+            newCommand = command + " || " + "echo " + lookFor
+            if lookFor in runCommand(newCommand):
+                return True
+            return False
+
+        if command[-1:] == ";":
+            self.command = command[:-1]
+            command = self.command
+
+        lookFor = generateStringNotInString(command)
+        if testPass(command, lookFor):
+            self.outputType = "Pass"
+        elif testFail(command, lookFor):
+            self.outputType = "Fail"
+        else:
+            self.outputType = "End"
+        return self.outputType
+
+    def __repr__(self):
+
+        return "[" + self.outputType + "]" + " " + self.command
+
+
+    def __init__(self, command):
+        self.command = command
+        self.getOutput(command)
+    
+    
+
 
 
 def getCombinations(givenList): # Get all possible combinations of a list
@@ -45,18 +94,24 @@ def dynamicTestingGenerator(input): #Where a list of tuples becomes an actual co
         self.assertFalse(Gilmour)
 
 
-    outputPings = len(input) * 2
+    outputPings = 0
+    for x in input:
+        outputPings += 2
+        if x.outputType == "End":
+            break
+            
+
 
     stringInput = ""
 
     for x in input:
-
+        command = x.command
         if not stringInput:
-            stringInput += x
-        elif x[-1:] == ";":
-            stringInput += " " + x
+            stringInput += command
+        elif command[-1:] == ";":
+            stringInput += " " + command
         else:
-            stringInput += "; " + x
+            stringInput += "; " + command
 
     while ";;" in stringInput: #ive been spending too long trying to figure out why ;; is happening. This should fix it.
         stringInput = stringInput.replace(";;", ";")
@@ -77,17 +132,20 @@ def main():
         print("Found " + str(len(data)) + " inputs.")
 
     print("\n" + str(len(INPUTS)) + " total inputs found")
+
+    INPUTS = [BaseCommand(x) for x in INPUTS]
     
     while len(argv) > 1: # unittest.main() gets mad if you pass in argv, so I pop all them off
         argv.pop()
 
-    COMMENTS = [x for x in INPUTS if "#"  in x] #Comments should not be in a chain of commands, because comments ruin everything
-    FOR_COMBINATIONS = list(set(INPUTS) - set(COMMENTS))
+    # COMMENTS = [x for x in INPUTS if "#"  in x] #Comments should not be in a chain of commands, because comments ruin everything
+    # FOR_COMBINATIONS = list(set(INPUTS) - set(COMMENTS))
+    FOR_COMBINATIONS = INPUTS
     COMBINATIONS = getCombinations(FOR_COMBINATIONS)
     print(str(len(FOR_COMBINATIONS)) + " of the inputs generated " + str(len(COMBINATIONS)) + " combinations.")
 
     
-    [COMBINATIONS.append(tuple([x]),) for x in COMMENTS]
+    # [COMBINATIONS.append(tuple([x]),) for x in COMMENTS]
 
     
 
@@ -100,6 +158,7 @@ def main():
         test_method = dynamicTestingGenerator(x)
         test_method.__name__ = "test_expected%d" + str(x) # If you try to change the name from test_expected%d IT BREAKS! MY GOD THIS TOOK ME AN HOUR TO FIGURE OUT
         setattr(DynamicTest, test_method.__name__, test_method)
+
 
 
 
